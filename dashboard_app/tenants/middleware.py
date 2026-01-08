c9e072f9052ede3ade5c36dc7a749cbc5cfea08d
Njaaga <jn.njaaga@gmail.com>
2026-01-08 11:59:28 -0500

    Update middleware.py

--- dashboard_app/tenants/middleware.py
+++ dashboard_app/tenants/middleware.py
@@ -23,7 +23,7 @@ class TenantMiddleware:
         "/media",
         "/api/forgot-password",
         "/api/reset-password",
-        "/api/users/invite",
+        "/api/set-password/",
     )
 
     def __init__(self, get_response):
