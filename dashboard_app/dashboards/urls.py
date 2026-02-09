be1debfa917aca3c18102867d3fe139ca91a0124
Njaaga <jn.njaaga@gmail.com>
2026-02-09 16:42:09 -0500

    Update urls.py

--- dashboard_app/dashboards/urls.py
+++ dashboard_app/dashboards/urls.py
@@ -16,7 +16,6 @@ from .views import (
     support_guest,
 )
 
-from oauth.quickbooks import quickbooks_connect, quickbooks_callback
 
 from django.contrib.auth import views as auth_views
 
@@ -55,7 +54,5 @@ urlpatterns = [
 
     # dataset adhoc execution endpoint
     path("datasets/run/", DatasetRunAdhocView.as_view(), name="datasets-adhoc-run"),
-    path("oauth/quickbooks/connect/", quickbooks_connect),
-    path("oauth/quickbooks/callback/", quickbooks_callback),
 
 ]
