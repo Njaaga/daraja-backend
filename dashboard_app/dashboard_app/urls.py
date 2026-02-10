b67fa7fcbdc60959289119faebac9d1ceaf756a6
Njaaga <jn.njaaga@gmail.com>
2026-02-09 20:17:21 -0500

    Update urls.py

--- dashboard_app/dashboard_app/urls.py
+++ dashboard_app/dashboard_app/urls.py
@@ -5,8 +5,8 @@ from subscriptions import views as sub_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('dashboards.urls')),
     path('api/tenants/', include('tenants.urls')),
+    path('api/', include('dashboards.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api/subscription/", include("subscriptions.urls")),
