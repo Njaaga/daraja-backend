784f332d39495e39477ead08e3f8030d5704d1b8
Njaaga <jn.njaaga@gmail.com>
2026-02-10 13:42:40 -0500

    Update urls.py

--- dashboard_app/dashboard_app/urls.py
+++ dashboard_app/dashboard_app/urls.py
@@ -1,52 +1,14 @@
-# dashboard_app/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-#from dashboards.oauth.quickbooks import (
- #   quickbooks_connect,
-  #  quickbooks_callback,
-#)
+from subscriptions import views as sub_views
 
 urlpatterns = [
-    # --------------------
-    # Admin
-    # --------------------
-    path("admin/", admin.site.urls),
-
-    # --------------------
-    # QuickBooks OAuth (PUBLIC)
-    # --------------------
- #   path(
-  #      "api/oauth/quickbooks/connect/",
-   #     csrf_exempt(quickbooks_connect),
-    #    name="quickbooks-connect",
-   # ),
-   # path(
-    #    "api/oauth/quickbooks/callback/",
-     #   csrf_exempt(quickbooks_callback),
-      #  name="quickbooks-callback",
-  #  ),
-
-    # --------------------
-    # Auth / tenants
-    # --------------------
-    path("api/tenants/", include("tenants.urls")),
-
-    # --------------------
-    # Dashboards app
-    # --------------------
-    path("api/", include("dashboards.urls")),
-
-    # --------------------
-    # JWT
-    # --------------------
-    path("api/token/", TokenObtainPairView.as_view()),
-    path("api/token/refresh/", TokenRefreshView.as_view()),
-
-    # --------------------
-    # Subscriptions
-    # --------------------
+    path('admin/', admin.site.urls),
+    path('api/', include('dashboards.urls')),
+    path('api/tenants/', include('tenants.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api/subscription/", include("subscriptions.urls")),
 ]
+
