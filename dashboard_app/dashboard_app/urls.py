a8afba76fda84154da4b72907d4fc033ca37e941
Njaaga <jn.njaaga@gmail.com>
2026-02-10 13:44:19 -0500

    Update urls.py

--- dashboard_app/dashboard_app/urls.py
+++ dashboard_app/dashboard_app/urls.py
@@ -5,6 +5,18 @@ from subscriptions import views as sub_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path(
+        "api/oauth/quickbooks/connect/",
+        csrf_exempt(quickbooks_connect),
+        name="quickbooks-connect",
+    ),
+    path(
+        "api/oauth/quickbooks/callback/",
+        csrf_exempt(quickbooks_callback),
+        name="quickbooks-callback",
+    ),
+    
     path('api/', include('dashboards.urls')),
     path('api/tenants/', include('tenants.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
