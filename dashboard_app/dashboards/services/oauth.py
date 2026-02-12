ea50637328a72cdcee30ad791c0284d66fbaf094
Njaaga <jn.njaaga@gmail.com>
2026-02-12 06:54:33 -0500

    Update oauth.py

--- dashboard_app/dashboards/services/oauth.py
+++ dashboard_app/dashboards/services/oauth.py
@@ -14,7 +14,7 @@ def refresh_quickbooks_token(api_source):
         ),
         headers={"Accept": "application/json"},
         data={
-            "grant_type": "oauth_refresh_token",
+            "grant_type": "refresh_token",
             "refresh_token": api_source.oauth_refresh_token,
         },
     )
