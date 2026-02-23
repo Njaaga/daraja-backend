1422078cc2f0c5135a784a8bb6651844bccff707
Njaaga <jn.njaaga@gmail.com>
2026-02-22 19:57:56 -0500

    Update dataset_runner.py

--- dashboard_app/dashboards/services/dataset_runner.py
+++ dashboard_app/dashboards/services/dataset_runner.py
@@ -5,7 +5,7 @@ def run_dataset(dataset, request):
     Must reuse existing dataset execution logic.
     DO NOT invent new logic here.
     """
-    from dashboards.views.datasets import DatasetViewSet
+    from dashboards.views import DatasetViewSet
 
     view = DatasetViewSet()
     view.request = request
