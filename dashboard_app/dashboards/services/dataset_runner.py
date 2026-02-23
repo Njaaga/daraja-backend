6b26581732ad6d2ea0720f181634302f112d88cb
Njaaga <jn.njaaga@gmail.com>
2026-02-22 19:57:27 -0500

    Create dataset_runner.py

---
+++ dashboard_app/dashboards/services/dataset_runner.py
@@ -0,0 +1,13 @@
+# dashboards/services/dataset_runner.py
+
+def run_dataset(dataset, request):
+    """
+    Must reuse existing dataset execution logic.
+    DO NOT invent new logic here.
+    """
+    from dashboards.views.datasets import DatasetViewSet
+
+    view = DatasetViewSet()
+    view.request = request
+
+    return view._run_dataset(dataset)
