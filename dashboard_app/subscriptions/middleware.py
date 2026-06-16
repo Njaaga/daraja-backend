ab38a7a5cc5e83445874b758002622b506f4f850
Njaaga <jn.njaaga@gmail.com>
2026-06-16 08:57:57 -0400

    Update middleware.py

--- dashboard_app/subscriptions/middleware.py
+++ dashboard_app/subscriptions/middleware.py
@@ -80,6 +80,8 @@ class SubscriptionEnforcementMiddleware(MiddlewareMixin):
         "/api/oauth/quickbooks/connect",
         "/api/kpis/",
         "/api/kpis",
+        "/api/kpis/executive/",
+        "/api/kpis/executive",
     ]
 
     def process_request(self, request):
