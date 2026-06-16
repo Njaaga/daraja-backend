49f1553d0791291b1c086f23e999f0dd9c61472c
Njaaga <jn.njaaga@gmail.com>
2026-06-16 07:40:55 -0400

    Update views.py

--- dashboard_app/kpis/views.py
+++ dashboard_app/kpis/views.py
@@ -1,1 +1,9 @@
+from rest_framework.viewsets import ModelViewSet
 
+from .models import KPI
+from .serializers import KPISerializer
+
+
+class KPIViewSet(ModelViewSet):
+    queryset = KPI.objects.all()
+    serializer_class = KPISerializer
