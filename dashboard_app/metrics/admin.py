3f409bc3aa6798d14d367a44772012506af12496
Njaaga <jn.njaaga@gmail.com>
2026-06-17 17:42:49 -0400

    Update admin.py

--- dashboard_app/metrics/admin.py
+++ dashboard_app/metrics/admin.py
@@ -1,1 +1,22 @@
+from django.contrib import admin
+from .models import MetricSnapshot
 
+
+@admin.register(MetricSnapshot)
+class MetricSnapshotAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "metric",
+        "tenant",
+        "value",
+        "recorded_at",
+    )
+
+    list_filter = (
+        "tenant",
+        "metric",
+    )
+
+    search_fields = (
+        "metric__name",
+    )
