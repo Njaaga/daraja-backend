b44a0c479fe8df13221935ca04f6ba28d89f0388
Njaaga <jn.njaaga@gmail.com>
2026-06-18 07:04:34 -0400

    Update urls.py

--- dashboard_app/dashboard_app/urls.py
+++ dashboard_app/dashboard_app/urls.py
@@ -29,9 +29,6 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api/subscription/", include("subscriptions.urls")),
     path("api/kpis/", include("kpis.urls")),
-    path(
-    "api/metrics/",
-    include("metrics.urls")
-),
+    path("api/metrics/", include("metrics.urls")),
 ]
 
