8479365e53b59b5e73c6dbfe196e4ff93a3542c7
Njaaga <jn.njaaga@gmail.com>
2026-06-18 06:57:54 -0400

    Create serializers.py

---
+++ dashboard_app/metrics/serializers.py
@@ -0,0 +1,16 @@
+# metrics/serializers.py
+
+from rest_framework import serializers
+from .models import Metric
+
+class MetricSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Metric
+        fields = [
+            "id",
+            "name",
+            "dataset",
+            "field_name",
+            "aggregation",
+        ]
