f2497a3474bfc1b1a10bd766a939fcc725e51fcf
Njaaga <jn.njaaga@gmail.com>
2026-06-18 05:56:48 -0400

    Update dataset_runner.py

--- dashboard_app/dashboards/services/dataset_runner.py
+++ dashboard_app/dashboards/services/dataset_runner.py
@@ -1,16 +1,17 @@
 # dashboards/services/dataset_runner.py
 
-if request is None:
-    print(
-        "[DATASET RUNNER] "
-        "Running without request context"
-    )
 def run_dataset(dataset, request):
     """
     Must reuse existing dataset execution logic.
     DO NOT invent new logic here.
     """
     from dashboards.views import DatasetViewSet
+
+    if request is None:
+        print(
+            "[DATASET RUNNER] "
+            "Running without request context"
+        )    
 
     view = DatasetViewSet()
     view.request = request
