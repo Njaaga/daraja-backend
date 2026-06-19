d55f34af8805245ade6fcc2899865101a66de689
Njaaga <jn.njaaga@gmail.com>
2026-06-19 10:06:06 -0400

    Update views.py

--- dashboard_app/metrics/views.py
+++ dashboard_app/metrics/views.py
@@ -1,7 +1,25 @@
+from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
-class MetricViewSet(APIView):
+from .models import Metric
+from .serializers import MetricSerializer
+from .services.trend_service import TrendService
 
-    def get(self, request):
-        return Response({"status": "ok"})
+
+class MetricViewSet(viewsets.ReadOnlyModelViewSet):
+
+    queryset = Metric.objects.all()
+    serializer_class = MetricSerializer
+
+    @action(detail=True, methods=["get"])
+    def trend(self, request, pk=None):
+
+        metric = self.get_object()
+
+        trend = TrendService.get_trend(
+            metric=metric,
+            days=30
+        )
+
+        return Response(trend)
